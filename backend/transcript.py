dcd6e02a8e41d8d6ae435633261359c0d24aa8c7
Senshiki <yvessenshiki@gmail.com>
2026-03-23 20:24:56 +0530

    got da transcript working y'all

--- backend/transcript.py
+++ backend/transcript.py
@@ -1,9 +1,21 @@
-from youtube_transcript_api import YouTubeTranscriptApi #importing the module
+#import the stuff and then call it
+from youtube_transcript_api import YouTubeTranscriptApi
 
-def main():
-    print("idk how ts concept works :D")
+def get_transcript(video_id):
+    ytt_api = YouTubeTranscriptApi()
+    transcript_list = ytt_api.list(video_id)
+    for transcript in transcript_list: # forgot to put the colon bahahahaha
+        print(
+        transcript.video_id, 
+        transcript.language,
+        transcript.is_generated,
+    )
+    fetched = transcript.fetch()
+    text = " ".join ([snippet.text for snippet in fetched])
+    return(text)
+
 
 if __name__ == "__main__":
-    main()
+    print(get_transcript('EI1ly8fzSmw'))
 
 
